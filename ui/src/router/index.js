c0fdc83941131b35620b5259f30504d33c35c75c
EliasHR <elias8579@gmail.com>
2024-09-24 15:07:19 +0400

    404

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -17,7 +17,7 @@ const router = createRouter({
         },
         {
             path: '/:pathMatch(.*)*',
-            beforeRouteEnter(to, from) {
+            beforeRouteUpdate(to, from) {
                 window.location.href = "/404"
             }
         }
