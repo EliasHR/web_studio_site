10114aa7c4839a6174d8d795002c4e5217e2fa2b
EliasHR <elias8579@gmail.com>
2024-09-24 12:37:46 +0400

    404

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -13,9 +13,20 @@ const router = createRouter({
         {
             path: '/:pathMatch(.*)*',
             name: '404',
-            component: Page404
+            component: Page404,
+            meta: { requires404: true }
         }
     ]
 })
+
+// Middleware для установки статуса 404
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requires404)) {
+        document.title = '404 Not Found';
+        next();
+    } else {
+        next();
+    }
+});
 
 export default router
