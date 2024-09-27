1b19c857396931e6f8759db82e2338ec23e17d66
EliasHR <elias8579@gmail.com>
2024-09-27 20:15:16 +0400

    Lazy router

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Page404 from '../views/Page404.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +6,12 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            component: () => import('@/views/HomeView.vue')
         },
         {
             path: '/404',
             name: '404',
-            component: Page404,
+            component: () => import('@/views/Page404.vue')
         },
         {
             path: '/:pathMatch(.*)*',
