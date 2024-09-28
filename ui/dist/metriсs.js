17f8178150f7b741b9b0130f286efba1b68baa2f
EliasHR <elias8579@gmail.com>
2024-09-28 16:01:10 +0400

    ssr

--- ui/dist/metriсs.js
+++
@@ -1,8 +0,0 @@
-/* < !--Yandex.Metrika counter-- > */
-
-/* <!-- /Yandex.Metrika counter --> */
-
- /*    < !--Google tag(gtag.js)-- > */
-{<script async src="https://www.googletagmanager.com/gtag/js?id=G-F26TTEQSY3"></script>
-<script><!-- Google tag (gtag.js) --></script> */}
-/* <!--Google tag(gtag.js)-- >
